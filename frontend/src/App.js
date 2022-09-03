263a9c87135421f95c5c4e76c04b498c19e2be13
kewenlok <kewen.lok@u.nus.edu>
2022-09-03 20:53:32 +0800

    Optimize routing mechanisms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import SignupPage from './components/SignupPage';
-import { Box } from '@mui/material';
+import Router from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <Box display={'flex'} flexDirection={'column'} padding={'4rem'}>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
-            <Route path="/signup" element={<SignupPage />} />
-          </Routes>
-        </Router>
-      </Box>
-    </div>
+    <>
+      <Router />
+    </>
   );
 }
 
