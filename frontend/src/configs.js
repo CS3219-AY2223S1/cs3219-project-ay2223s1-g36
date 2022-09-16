a4ce3ce58424d144db2a5b51f038cfb2cb3d6dcd
johan9se <johannine.enerio@gmail.com>
2022-09-16 13:19:50 +0800

    Add logout handler on frontend

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -4,8 +4,11 @@ const PREFIX_USER_SVC = '/api/user';
 
 export const URL_USER_SVC = URI_USER_SVC + PREFIX_USER_SVC;
 
+export const URL_USER_SVC_REG = URL_USER_SVC + '/register';
 export const URL_USER_SVC_LOGIN = URL_USER_SVC + '/login';
 export const URL_USER_SVC_LOGOUT = URL_USER_SVC + '/logout';
+
+export const URL_USER_SVC_AUTH = URL_USER_SVC + '/auth';
 export const URL_USER_SVC_DELETE = URL_USER_SVC + '/deleteAccount';
 export const URL_USER_SVC_UPDATEPW = URL_USER_SVC + '/updatePassword';
 
