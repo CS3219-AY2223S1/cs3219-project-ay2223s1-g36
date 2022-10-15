d5eb13ad2e86bb3617b69f6e33f1845b4f10af59
DESKTOP-IU3KFL5\limyb <limyb50@gmail.com>
2022-10-15 17:04:22 +0800

    Establish question service: add functionality to find questions for selected difficulty

---
+++ question-service/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import { getQues } from './controller/question-controller.js';
+import { getQuesForDifficulty } from './controller/question-controller.js';
+
+const app = express();
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+// app.use(cors()) // config cors so that front-end can use
+app.use(cors({
+    origin : "http://localhost:3000",
+    credentials: true,
+  }))
+
+const router = express.Router()
+
+// Controller will contain all the User-defined Routes
+router.get('/', (_, res) => res.send('Hello World from user-service'))
+
+app.use('/api/question', router).all((_, res) => {
+    res.setHeader('content-type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+})
+
+router.get('/getQues', getQues)
+router.get('/getQuesForDifficulty', getQuesForDifficulty)
+
+app.listen(8001, () => console.log('question-service listening on port 8001'));
