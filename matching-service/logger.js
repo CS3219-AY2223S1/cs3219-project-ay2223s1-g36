1a61dc0426ce152bdec91a9bb98f23df47c287ab
CookieHoodie <cookiehoodie9@gmail.com>
2022-10-24 18:04:21 +0800

    Remove comments

--- matching-service/logger.js
+++ matching-service/logger.js
@@ -14,10 +14,6 @@ const logger = winston.createLogger({
   ],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 logger.add(new winston.transports.Console({
   format: winston.format.combine(
       winston.format.colorize(),
