175cf0204b8f20e8975995af6cdf6c215e342251
DESKTOP-IU3KFL5\limyb <limyb50@gmail.com>
2022-10-26 23:54:13 +0800

    Update bash script to run history service

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ docker-compose -f question-service/docker-compose.yaml up -d
 docker-compose -f question-service/docker-compose.yaml logs -f -t &
 docker-compose -f collab-service/docker-compose.yaml up &
 docker-compose -f matching-service/docker-compose.yaml up &
-docker-compose -f user-service/docker-compose.yaml up
+docker-compose -f user-service/docker-compose.yaml up &
+docker-compose -f history-service/docker-compose.yaml up
