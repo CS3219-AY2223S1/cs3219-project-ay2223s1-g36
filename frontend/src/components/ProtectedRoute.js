537cb203b8c6e533483f66d466aab751ea3415f7
johan9se <johannine.enerio@gmail.com>
2022-10-28 12:29:09 +0800

    add call to authenticate user in frontend

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -2,8 +2,8 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
 export const ProtectedRoute = ({ children, redirectPath = '/welcome/login' }) => {
-  const { user } = useAuth();
-  if (!user) {
+  const { user, checkAuth } = useAuth();
+  if (!user || !checkAuth(user)) {
     return <Navigate replace to={redirectPath} />;
   }
   return children;
