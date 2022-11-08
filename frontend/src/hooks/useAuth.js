a01d0255abdfc36cfd35992a426988fc328041da
johan9se <johannine.enerio@gmail.com>
2022-11-08 10:55:16 +0800

    Add call to user service auth for all Protected Routes

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,11 +1,29 @@
-import { createContext, useContext, useMemo } from 'react';
+import { createContext, useContext, useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLocalStorage } from './useLocalStorage';
+import axios from 'axios';
+import { URL_USER_SVC_AUTH } from '../configs';
+import { STATUS_CODE_OK, STATUS_CODE_UNAUTH } from '../constants';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useLocalStorage('user', null);
   const navigate = useNavigate();
+
+  // call this function when you want to authenticate the user
+  const checkAuth = async (token) => {
+    const res = await axios
+      .post(URL_USER_SVC_AUTH, { token }, { withCredentials: true })
+      .catch((err) => {
+        if (err.response.status === STATUS_CODE_UNAUTH) {
+          setUser(null);
+          localStorage.clear();
+        }
+      });
+    if (res && res.status === STATUS_CODE_OK) {
+      console.log('User still auth, proceed.');
+    }
+  };
 
   const login = async (data) => {
     document.cookie = 'token=' + data.token;
@@ -22,11 +40,18 @@ export const AuthProvider = ({ children }) => {
   const value = useMemo(
     () => ({
       user,
+      checkAuth,
       login,
       logout
     }),
     [user]
   );
+
+  useEffect(() => {
+    if (user) {
+      checkAuth(user.token);
+    }
+  }, []);
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
